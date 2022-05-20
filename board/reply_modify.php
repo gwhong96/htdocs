2b8d2f0c60eadc44f0c6745068ba98e5a85f6fb0
gwhong96 <gwhong96@gmail.com>
2022-05-20 18:15:19 +0900

    commit0520

--- board/reply_modify.php
+++ board/reply_modify.php
@@ -1,4 +1,4 @@
-<!--댓글 수정 및 삭제 -->
+<!--댓글 수정 -->
 <?php
   include $_SERVER['DOCUMENT_ROOT'].'./board/session.php';
   include $_SERVER['DOCUMENT_ROOT'].'./board/connectDB.php';
@@ -9,11 +9,11 @@
   $date       = date("Y-m-d H:i:s");
 
   $sql  = "UPDATE reply SET ";
-  $sql .= "reply = (?) ";
+  $sql .= "reply = (?), replyDate = (?)";
   $sql .= "WHERE replyID = (?) AND boardID = (?)";
 
   $query = $dbConnect -> prepare($sql);
-  $query -> bind_param('sii', $reply, $replyID, $boardID);
+  $query -> bind_param('ssii', $reply, $date, $replyID, $boardID);
   $result = $query -> execute();
 
   echo "<script type='text/javascript'>alert('댓글 저장 완료');window.location = './view.php?boardID={$boardID}';</script>";
