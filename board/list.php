b7eed6ef06abc6d120d8a230ae200dfcded8788a
gwhong96 <gwhong96@gmail.com>
2022-05-30 14:02:01 +0900

    commit

--- board/list.php
+++ board/list.php
@@ -28,9 +28,9 @@
 <h3><a href="./list.php"><img src="../img/logo.svg"/></a></h3>
 <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Q&A</h4>
 <div class="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
-  <button type="button" style="background : gray" class="items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" onclick="location.href='./write.php';">게시글 작성</button>
-  <button type="button" style="background : gray" class="items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" onclick="location.href='./signOut.php';">로그아웃</button>
-  <button type="button" style="background : gray" class="items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" onclick="location.href='./signUpForm.php';">회원정보 수정</button>
+  <button type="button" style="background : gray; margin-left : 10%" class="items-center px-4 py-2 text-sm font-medium text-white rounded-lg" onclick="location.href='./write.php';">게시글 작성</button>
+  <button type="button" style="background : gray; margin-left : 30%" class="items-center px-4 py-2 text-sm font-medium text-white rounded-lg" onclick="location.href='./signOut.php';">로그아웃</button>
+  <button type="button" style="background : gray" class="items-center px-4 py-2 text-sm font-medium text-white rounded-lg" onclick="location.href='./signUpForm.php';">회원정보 수정</button>
   <div style="padding-left:100px; padding-right:100px" class="w-full overflow-x-auto">
 		<table style = "width : 70%" class="w-full whitespace-no-wrap">
       <thead>
@@ -128,24 +128,22 @@
                   // $title = htmlspecialchars($memberInfo['title']);
                   echo "<tr class='text-gray-700 dark:text-gray-400'>";
                   echo "<td class='th_sty px-4 py-3'>";
-                  echo "<div class='flex items-center text-sm'>";
-                  echo "<div><p class='font-semibold'>";
-
-                  echo ($totalCount['count(boardID)']-$i-(($page-1)*$numView))."</p></div>";
+                  echo "<div class='px-2 py-1'><p class='font-semibold'>";
+                  echo ($totalCount['count(boardID)']-$i-(($page-1)*$numView))."</p>";
                   echo "</div></td>";//총 게시글 수 boardID와 무관하게 역순정렬
 
                   if($memberInfo['disYN'] == 'N'){//비공개 일 시
-                      echo "<td class='th_sty px-4 py-3'><a href = '../board/checkDisYN.php?boardID={$memberInfo['boardID']}' style='float:left'>";//게시글 비밀번호 확인 페이지
+                      echo "<td class='th_sty px-4 py-3 text-gray-500'><a href = '../board/checkDisYN.php?boardID={$memberInfo['boardID']}' style='float:left'>";//게시글 비밀번호 확인 페이지
                       echo "<img src = '../img/lock.png' title = 'lock' width = '20' height ='20' style='float:left'/>";
                   }else{
-                      echo "<td class='px-4 py-3'><a href = '../board/view.php?boardID={$memberInfo['boardID']}'>";//상세보기 페이지 이동링크
+                      echo "<td class='px-4 py-3 text-gray-500'><a href = '../board/view.php?boardID={$memberInfo['boardID']}'>";//상세보기 페이지 이동링크
                   }
                   echo $memberInfo['title'];
                   echo "</a></td>";
-                  echo "<td class='th_sty px-4 py-3 text-sm'>{$memberInfo['nickName']}</td>";
-                  echo "<td class='th_sty px-4 py-3 text-xs'>{$memberInfo['regTime']}</td>";
-                  echo "<td class='th_sty px-4 py-3 text-sm'>{$memberInfo['views']}</td>";
-                  echo "<td class='th_sty px-4 py-3 text-sm'><a href='history.php?boardID={$memberInfo['boardID']}' target = '_blank' onclick='window.open(this.href,'팝업창','width=100, height=100');'>log<a/></td>";
+                  echo "<td class='th_sty text-gray-500 px-4 py-3 text-sm'>{$memberInfo['nickName']}</td>";
+                  echo "<td class='th_sty text-gray-500 px-4 py-3 text-xs'>{$memberInfo['regTime']}</td>";
+                  echo "<td class='th_sty text-gray-500 px-4 py-3 text-sm'>{$memberInfo['views']}</td>";
+                  echo "<td class='th_sty text-gray-500 px-4 py-3 text-sm'><a href='history.php?boardID={$memberInfo['boardID']}' target = '_blank' onclick='window.open(this.href,'팝업창','width=100, height=100');'>log<a/></td>";
                   echo "</tr>";
                 }
               }else{
@@ -158,10 +156,10 @@
   </div>
 
 
-  <div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800" style="text-align : center">
-    <?php
-      include $_SERVER['DOCUMENT_ROOT'].'../board/paging.php';
-     ?>
+  <div class="grid px-4 py-3 text-xs font-semibold text-gray-500 uppercase sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800" style="text-align : center">
+
+
+    <?php include $_SERVER['DOCUMENT_ROOT'].'../board/paging.php'; ?>
 </div>
 <?php include $_SERVER['DOCUMENT_ROOT'].'../board/search.php';?>
 </div>
